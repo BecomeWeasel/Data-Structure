586918933a105adac7a38c6076dd6850f75e344e
황성우 <hsw0194@hanyang.ac.kr>
2019-05-09 19:22:42 +0900

    Modify InsertEdge function

--- Lab9/p9_1.c
+++ Lab9/p9_1.c
@@ -27,6 +27,8 @@ void InsertEdge(Graph G, int a, int b);
 
 void DeleteGraph(Graph G);
 
+void PrintGraph(Graph G);
+
 int main(int argc, char **argv)
 {
 	FILE *fp = fopen(argv[1], "r");
@@ -40,75 +42,86 @@ int main(int argc, char **argv)
 	// size=somevalue;
 
 	int phase = 0;
-	while (fgets(bufferforcount, sizeof(bufferforcount), fp))
+	fgets(bufferforcount, 128, fp);
+	strtok(bufferforcount, "\n");
+
+	strcpy(buffer2, bufferforcount);
+
+	char *tempstr;
+
+	tempstr = strtok(bufferforcount, " ");
+	size++;
+
+	while (1)
 	{
-		phase++;
-		strtok(bufferforcount, "\n");
-		if (phase == 1)
-		{
-			strcpy(buffer2, bufferforcount);
-
-			char *tempstr;
-
-			tempstr = strtok(bufferforcount, " ");
-			size++;
-
-			while (1)
-			{
-				tempstr = strtok(NULL, " ");
-				if (tempstr == NULL) break;
-				size++;
-			}
-
-
-			tempstr = NULL;
-
-			int *setOfNodes = (int *) malloc(sizeof(int) * size);
-
-
-
-			char *tempstr2 = strtok(buffer2, " ");
-			setOfNodes[0] = atoi(tempstr2);
-
-			for (int j = 1; j < size; j++)
-			{
-				tempstr2 = strtok(NULL, " ");
-				setOfNodes[j] = atoi(tempstr2);
-			}
-
-			myGraph = CreateGraph(setOfNodes, size);
-		}
-		else if (phase == 2)
-		{
-
-			// 입력이 두줄만 온다고 가정?
-			char *edgestr = NULL;
-
-			while (1)
-			{
-				edgestr = strtok(edgestr == NULL ? bufferforcount : NULL, " ");
-				if(edgestr==NULL) break;
-				InsertEdge(myGraph,edgestr[0]-'0',edgestr[2]-'0');
-			}
-		}
+		tempstr = strtok(NULL, " ");
+		if (tempstr == NULL) break;
+		size++;
 	}
-	// print matrix
-	printf("  ");
-	for(int i=0;i<size;i++){
-		printf("%d ",myGraph->node[i]);
-	}
-	printf("\n");
 
-	for(int k=0;k<size;k++){
-		printf("%d ",myGraph->node[k]);
-		for(int j=0;j<size;j++){
-			printf("%d ",myGraph->matrix[k][j]);
-		}
-		printf("\n");
-	}
-	printf("\n");
 
-	//
+	tempstr = NULL;
+
+	int *setOfNodes = (int *) malloc(sizeof(int) * size);
+
+
+	char *tempstr2 = strtok(buffer2, " ");
+	setOfNodes[0] = atoi(tempstr2);
+
+	for (int j = 1; j < size; j++)
+	{
+		tempstr2 = strtok(NULL, " ");
+		setOfNodes[j] = atoi(tempstr2);
+	}
+
+	myGraph = CreateGraph(setOfNodes, size);
+	free(setOfNodes);
+
+
+	int src;
+	int dest;
+	while (fscanf(fp, "%d", &src) != EOF)
+	{
+		fscanf(fp, "-");
+		fscanf(fp, "%d ", &dest);
+		InsertEdge(myGraph,src,dest);
+	}
+
+
+	// 입력이 두줄만 온다고 가정?
+
+
+//			while (1)
+//			{
+//
+//				int src;
+//
+//
+//				edgestr = strtok(edgestr == NULL ? bufferforcount : NULL," ");
+//				if(edgestr==NULL) break;
+//
+//				char Edgestore[128]="";
+//
+//				strcpy(Edgestore,edgestr);
+//
+//				char EdgeStrFirst[128]="";
+//				char EdgeStrSecond[128]="";
+//				char * tempstr=strtok(Edgestore,"-");
+//				strcpy(EdgeStrFirst,tempstr);
+//				tempstr=strtok(NULL,"-");
+//				strcpy(EdgeStrSecond,tempstr);
+//
+//
+//				InsertEdge(myGraph,atoi(EdgeStrFirst),atoi(EdgeStrSecond));
+//				printf("%s\n",edgestr);
+//			}
+
+
+
+// print matrix
+	PrintGraph(myGraph);
+
+//
 	DeleteGraph(myGraph);
 }
 
@@ -130,9 +143,11 @@ Graph CreateGraph(int *input_nodes, int numberOfNode)
 		target->matrix[i] = (int *) malloc(numberOfNode * sizeof(int));
 	}
 
-	for(int i=0;i<numberOfNode;i++){
-		for(int j=0;j<numberOfNode;j++){
-			target->matrix[i][j]=0;
+	for (int i = 0; i < numberOfNode; i++)
+	{
+		for (int j = 0; j < numberOfNode; j++)
+		{
+			target->matrix[i][j] = 0;
 		}
 
 	}
@@ -163,4 +178,25 @@ void DeleteGraph(Graph G)
 	free(G->matrix);
 	free(G->node);
 	free(G);
+}
+
+void PrintGraph(Graph G)
+{
+	printf("  ");
+	for (int i = 0; i < G->size; i++)
+	{
+		printf("%d ", G->node[i]);
+	}
+	printf("\n");
+
+	for (int k = 0; k < G->size; k++)
+	{
+		printf("%d ", G->node[k]);
+		for (int j = 0; j < G->size; j++)
+		{
+			printf("%d ", G->matrix[k][j]);
+		}
+		printf("\n");
+	}
+	printf("\n");
 }
